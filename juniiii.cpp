a32601198b5870e2ecd2caece3bb4e1fa3f68490
iknbhsbdvhbeu <158805058+iknbhsbdvhbeu@users.noreply.github.com>
2024-02-06 13:56:15 +0800

    Add files via upload

---
+++ juniiii.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "untuk juni" << endl;
+
+    string kata;
+    cout << "Masukkan kata-kata yang sering oji pakai : ";
+    
+    while (true) {
+        cin >> kata;
+
+        if (kata == "selesai") {
+            break;
+        }
+
+        cout << "Hai dede  " << endl;
+         cout << " wanita yang nda sengaja ku temui karena tugas negara hahaha" << endl;
+    cout << " nih oji mau bilang sesuatu, kalau di bilangin itu di dengar. jangan bawel" << endl;
+    cout << " disuruh tidur cepat tuuh langsung tidurr, mamnya juga jangan telat telat nanti jadinya kayak kemarin malam" << endl;
+    cout << " sama 1 lagi , kurang2i ngegantung oji hahaha" << endl;
+    cout << "ditunggu di tanggal 22 februari 2024 ya dede" << endl;
+    cout << "wapyuuu <3 xixi"
+    ;}
+
+    cout << " hai dedee, wanita yang nda sengaja ku temui karena tugas negara hahaha" << endl;
+    cout << " nih oji mau bilang sesuatu, kalau di bilangin itu di dengar. jangan bawel" << endl;
+    cout << " disuruh tidur cepat tuuh langsung tidurr, mamnya juga jangan telat telat nanti jadinya kayak kemarin malam" << endl;
+    cout << " sama 1 lagi , kurang2i ngegantung oji hahaha" << endl;
+    cout << "ditunggu di tanggal 22 februari 2024 ya dede" << endl;
+    cout << "wapyuuu <3 xixi"
+
+"return";0;
+}
+
